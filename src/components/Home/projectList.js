a242427b4854f74402b5066c60b86b5926939f31
jyoon <wldbszp@gmail.com>
2022-10-17 20:30:26 +0900

    Chore: 프로젝트 리스트 추가

--- src/components/Home/projectList.js
+++ src/components/Home/projectList.js
@@ -1,7 +1,11 @@
 export const projectList = [
   {
     title: "영화, TV 홈페이지 제작",
-    desc: "TMDB Api를 사용하여 목록, 상세페이지와 검색 기능을 구현하였습니다.",
+    desc: [
+      "api를 모듈화하고 react-query로 데이터 패칭",
+      "반복되는 카드 영역은 코드 재사용을 위해 컴포넌트 분리",
+      "mui의 Rating 컴포넌트를 사용하여 간편하게 별점 구현",
+    ],
     stack: [
       "React",
       "Typescript",
@@ -16,14 +20,21 @@ export const projectList = [
   },
   {
     title: "React Router 기능 구현",
-    desc: "React와 History API를 사용하여 React-router-dom 기능을 구현하였습니다.",
-    stack: ["React", "Javascript"],
+    desc: [
+      "context API를 사용하여 하위 컴포넌트에서 주소 변경이 일어날 경우 렌더링 될 수 있도록 전역상태관리 ",
+      "History API를 사용하여 React-router-dom 기능을 구현",
+    ],
+    stack: ["React", "Javascript", "context Api"],
     homepage: "https://spa-router.netlify.app/",
     git: "https://github.com/wldbszpflrxj/react-router",
   },
   {
     title: "투자 관리 서비스의 관리자 기능 구현",
-    desc: "Api 호출을 통해 계좌와 사용자의 목록을 보여주고 수정, 삭제 가능한 관리자페이지를 구현하였습니다.",
+    desc: [
+      "server json의 search api를 사용하여 검색 기능을 구현",
+      "query string으로 데이터를 전달하여 사용자의 편의성 고려",
+      "ID 기반으로 데이터 매칭이 필요한 값은 재사용성을 위해 hook 생성",
+    ],
     stack: [
       "React",
       "Json-server Api",
@@ -35,28 +46,57 @@ export const projectList = [
     git: "https://github.com/wldbszpflrxj/6th_investment-admin",
   },
   {
-    title: "빙그레 웹사이트 리뉴얼 구축",
-    desc: "스프링부트 템플릿을 사용하여 빙그레 기업 공식 웹사이트를 리뉴얼 제작하였습니다.",
-    stack: ["Thymeleaf", "Jquery", "Ajax", "stock Api", "css"],
-    homepage: "https://www.bing.co.kr/#",
-  },
-  {
-    title: "뮤자인 리뉴얼 구축",
-    desc: "Canvas로 동적인 라인 인터렉션을 구현하여 홈페이지를 리뉴얼 제작하였습니다. ",
-    stack: ["Thymeleaf", "JQuery", "Canvas", "Gsap", "css"],
-    git: "https://musign.net/",
-  },
-  {
-    title: "SPC 기업사이트 리뉴얼 구축",
-    desc: "워드프레스를 기반으로 브랜드, 계열사를 통합 관리할 수 있도록 리뉴얼 제작하였습니다.",
-    stack: [
-      "Wordpress",
-      "JQuery",
-      "Axios",
-      "Redux",
-      "youtube Api",
-      "Instagram Api",
+    title: "깃헙 레파지토리 이슈 관리",
+    desc: [
+      "react-markdown을 사용하여 마크다운 렌더링",
+      "mui 컴포넌트를 사용하여 반응형 대응",
     ],
-    git: "https://www.spc.co.kr/",
+    stack: ["React", "github Api", "Axios", "context Api", "mui"],
+    homepage: "https://github-issue-viewer-team2.netlify.app/",
+    git: "https://github.com/wldbszpflrxj/4th_github-issue-viewer/",
   },
+  {
+    title: "댓글 목록 CRUD",
+    desc: [
+      "useState를 사용하여 페이지네이션 구현",
+      "page와 limit 값을 활용하여 api 호출",
+    ],
+    stack: ["React", "Json-server Api ", "Axios", "Redux thunk", "emotion"],
+    git: "https://github.com/wldbszpflrxj/5th_comments-list",
+  },
+  {
+    title: "프루떼 스토어 리뉴얼",
+    desc: [
+      "Redux-persist 기능을 Api 대체 기능으로 사용하여 로컬스토리지에 데이터를 저장",
+      "Redux를 통해 상품 관리, 주문 상태 관리 ",
+    ],
+    stack: ["React", "Axios", "Redux", "mui", "emotion"],
+    homepage: "https://fruitte-renewal.netlify.app/",
+    git: "https://github.com/wldbszpflrxj/2nd_fruitte",
+  },
+  // {
+  //   title: "빙그레 웹사이트 리뉴얼 구축",
+  //   desc: "스프링부트 템플릿을 사용하여 빙그레 기업 공식 웹사이트를 리뉴얼 제작하였습니다.",
+  //   stack: ["Thymeleaf", "Jquery", "Ajax", "stock Api", "css"],
+  //   homepage: "https://www.bing.co.kr/#",
+  // },
+  // {
+  //   title: "뮤자인 리뉴얼 구축",
+  //   desc: "Canvas로 동적인 라인 인터렉션을 구현하여 홈페이지를 리뉴얼 제작하였습니다. ",
+  //   stack: ["Thymeleaf", "JQuery", "Canvas", "Gsap", "css"],
+  //   git: "https://musign.net/",
+  // },
+  // {
+  //   title: "SPC 기업사이트 리뉴얼 구축",
+  //   desc: "워드프레스를 기반으로 브랜드, 계열사를 통합 관리할 수 있도록 리뉴얼 제작하였습니다.",
+  //   stack: [
+  //     "Wordpress",
+  //     "JQuery",
+  //     "Axios",
+  //     "Redux",
+  //     "youtube Api",
+  //     "Instagram Api",
+  //   ],
+  //   git: "https://www.spc.co.kr/",
+  // },
 ];
